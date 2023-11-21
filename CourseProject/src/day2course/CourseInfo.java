cb5ce76346fafa1a72d0e664d17ca90310da9bbb
shivamdinde <shivamdinde60@gmail.com>
2023-11-21 19:31:32 +0530

    changes21nov

---
+++ CourseProject/src/day2course/CourseInfo.java
@@ -0,0 +1,27 @@
+package day2course;
+
+public class CourseInfo {
+	private String ProviderName;
+	private int Duration;
+	
+	
+	public String getProviderName() {
+		return ProviderName;
+	}
+	public void setProviderName(String providerName) {
+		ProviderName = providerName;
+	}
+	public int getDuration() {
+		return Duration;
+	}
+	public void setDuration(int duration) {
+		Duration = duration;
+	}
+	
+	public CourseInfo(String providerName, int duration) {
+		super();
+		ProviderName = providerName;
+		Duration = duration;
+	}
+	
+}
